36b1764b9c45ed387ab03d875cd5f6cc38e25ac4
Payton <thenumber46853@gmail.com>
2018-09-13 16:41:12 -0700

    Add pure prompt and highlighting

--- fresh_install.sh
+++ fresh_install.sh
@@ -4,15 +4,15 @@ sudo apt update
 sudo apt upgrade -y
 
 sudo apt install build-essential software-properties-common software-properties-gtk \
-	         python-software-properties git vim-nox tmux zsh -y
+python-software-properties git vim-nox tmux zsh python3.6 python3-distutils -y
+
+# Install pip3
+curl https://bootstrap.pypa.io/get-pip.py | sudo python3.6
 
 # Install NodeJS
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt update
 sudo apt install -y nodejs
-
-# Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install VSCode
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -31,6 +31,21 @@ sudo npm install --global pure-prompt --allow-root --unsafe-perm=true
 # Change the default shell to zsh
 chsh -s $(which zsh)
 
+# Set pure-prompt stuff in .zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME=""/g' $HOME/.zshrc
+echo "# Setup pure-prompt" >> $HOME/.zshrc
+echo "autoload -U promptinit; promptinit" >> $HOME/.zshrc
+echo "prompt pure"
+
 # Generate an RSA public and private key
 ssh-keygen -t rsa -b 4096 -P "" -f "$HOME/.ssh/id_rsa" -q
 
+# Install Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install zsh syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Add it to our plugins
+sed -i 's/^plugins=(/plugins=(\n\tzsh-syntax-highlighting/g' $HOME/.zshrc
+source $HOME/.zshrc
